d0576d30dd33a5d63cb9e0eca0b56df0684aba1d
kevinpemonon <kevinpemonon@gmail.com>
2021-06-27 22:35:51 +0200

    fix some recommendations lgtm

--- back-end/api/src/controllers/CategoryController.js
+++ back-end/api/src/controllers/CategoryController.js
@@ -1,11 +1,4 @@
 const CategoryService = require('../services/CategoryService');
-const { QueryTypes } = require("sequelize");
-const Sequelize = require("sequelize");
-
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-    dialect: 'postgres',
-    host: process.env.DB_HOST
-});
 
 module.exports = {
 
